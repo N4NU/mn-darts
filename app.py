eb4d1fbfc4da629a136cad7cd6b5afcf29f91ba5
N4NU <kazukif12582552@gmail.com>
2017-07-12 22:19:23 +0900

    ログイン機能のプロトタイプ

---
+++ app.py
@@ -0,0 +1,79 @@
+"""Flask Login Example and instagram fallowing find"""
+
+from flask import Flask, url_for, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+from flask.ext.bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config.update(
+    DEBUG=True,
+    SQLALCHEMY_DATABASE_URI='sqlite:///test.db'
+)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+class User(db.Model):
+    """ Create user table"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    """ Session control"""
+    if not session.get('logged_in'):
+        return render_template('index.html')
+    else:
+        if request.method == 'POST':
+            username = request.form['username']
+            return render_template('index.html', data=username)
+        return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Login Form"""
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        name = request.form['username']
+        passwd = request.form['password']
+        try:
+            data = User.query.filter_by(username=name).first()
+            if data is None:
+                return 'Dont Login'
+            if bcrypt.check_password_hash(data.password, passwd):
+                session['logged_in'] = True
+                return redirect(url_for('home'))
+        except:
+            return "Dont Login"
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    """Register Form"""
+    if request.method == 'POST':
+        hashed_pass = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+        new_user = User(username=request.form['username'], password=hashed_pass)
+        db.session.add(new_user)
+        db.session.commit()
+        return render_template('login.html')
+    return render_template('register.html')
+
+@app.route("/logout")
+def logout():
+    """Logout Form"""
+    session['logged_in'] = False
+    return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    app.debug = True
+    db.create_all()
+    app.secret_key = "123"
+    app.run(host='0.0.0.0')
+    
